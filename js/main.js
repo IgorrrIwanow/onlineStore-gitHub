6d3f95054e7dc4fd629a8918ebdd794d8f231b49
Igor <igorrriwanow@yandex.ru>
2021-08-23 16:16:21 +0300

    added function sumPrices

--- js/main.js
+++ js/main.js
@@ -1,36 +1,81 @@
-const products = [{
-        id: 1,
-        title: 'Notebook',
-        price: 2000
-    },
-    {
-        id: 2,
-        title: 'Mouse',
-        price: 20
-    },
-    {
-        id: 3,
-        title: 'Keyboard',
-        price: 200
-    },
-    {
-        id: 4,
-        title: 'Gamepad',
-        price: 50
+class ProductList{
+    constructor(){
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+        this.sum = this._sumPrices();
     }
-];
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    render(){
+        const block = document.querySelector('.products');
+        for(let product of this.goods){
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend",item.render());
+        }
+    }
+    _sumPrices() {
+        let sum = 0;
+        for (let product of this.goods) {
+            sum += product.price;
+        }
+        return sum;
+    }
+}
 
-const renderProduct = (item) =>
-    `<div class="product-item">
-        <h3 class="product-item_title">${item.title}</h3>
-        <img src="../img/computer.jpg" class="product-item_img" width="150" height="150"></img>
-        <p class="product-item_price">Цена товара: ${item.price} &#8381;</p>
-        <button class="buy-btn">Купить</button>
-    </div>`;
+class ProductItem{
+    constructor(product, img='../img/computer.jpg'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render(){
+           return `<div class="product-item">
+                <h3 class="product-item_title">${this.title}</h3>
+                <img src="../img/computer.jpg" class="product-item_img" width="150" height="150"></img>
+                 <p class="product-item_price">Цена товара: ${this.price} &#8381;</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
 
-const renderPage = list => {
-    const productList = list.map(item => renderProduct(item));
-    document.querySelector('.products').innerHTML = productList.join('');
-};
+class Basket {
+    constructor() {
 
-renderPage(products);
+    }
+    
+    clearBasket() {
+
+    } //Функция очищает корзину
+    showAllGoodsPrice() {
+
+    } //Функция высчитывает и показывает общую стоимость добавленных товаров
+}
+
+class ShopItem {
+    constructor() {
+
+    }
+
+    addInBasket() {
+
+    } //Функция добавляет товар в корзину, меняет его кол-во и цену(если добавлено больше одного товара)
+
+    deleteItemFromBasket() {
+
+    } //Функция удаляет товар из корзины или меняет кол-во и цену (если было добавлено более одного товара)
+
+    renderItem() {
+
+    } //Функция возвращает HTML разметку для товара
+
+}
+
+let list = new ProductList();
