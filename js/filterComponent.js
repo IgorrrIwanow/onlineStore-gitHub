eebd1e4afd93347e31d2a944789a3a644b728a99
Igor <igorrriwanow@yandex.ru>
2021-09-08 17:17:31 +0300

    added new components

---
+++ js/filterComponent.js
@@ -0,0 +1,8 @@
+Vue.component('filter_el', {
+    template: ` <form action="#" class="search-form" @submit.prevent="$root.filter">
+                    <input type="text" class="search-field" v-model="$root.userSearch">
+                    <button class="btn-search" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>`
+});
